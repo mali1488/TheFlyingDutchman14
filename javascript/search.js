d770f78fe4ddbcc13743b6e8edb7d810330107b5
Mattias <mattias.linder87@gmail.com>
2015-02-14 12:39:24 +0100

    better search function

--- javascript/search.js
+++ javascript/search.js
@@ -1,29 +1,4 @@
 /*
-  If Substring is a proper prefix of String, it is added to BeerList.
-*/
-function searchString(String,SubString,BeerList){
-    var length = String.length;
-    var bool = true;
-    String = String.toLowerCase();
-    SubString = SubString.toLowerCase();
-    for(var i = 0; i<length;i++){
-	if(String == []) {
-	    return;
-	}
-	if(String[i] === SubString[i]) {
-	    continue;
-	} else {
-	    bool = false
-	    break;
-	}
-    }
-    if(bool === true) {
-	BeerList.push(SubString);
-    }
-    return;
-}
-
-/*
   This functions search the database API for a beer
   prefix that matches the String parameter.  
  */
@@ -33,12 +8,21 @@ function searchBar(String,Username,Password) {
 	var beerMatched = [];
 	var length = data.payload.length;
 	var subString;
+
 	for(var i = 0; i<length; i++) {
-	    searchString(String, data.payload[i].namn,beerMatched);
+	    subString = data.payload[i].namn;
+	    
+	    if(data.payload[i].namn === "" || data.payload[i].namn === []){
+	    } else if(subString.toLowerCase().search(String.toLowerCase()) != -1) {
+		beerMatched.push(subString);
+	    }
+
 	}
+
 	var HTML = "";
 	length = beerMatched.length;
 	for(var i = 0; i<length; i++) {
+	    // Ugly html code
 	    HTML = HTML + "<a href=\"https://www.google.se\">" + beerMatched[i] +"</a><br>";
 	}
 	$('.ListOfBeers').html(HTML);
