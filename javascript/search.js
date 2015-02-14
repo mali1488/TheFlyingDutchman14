73ff5428ccdd6b351cff2de21e6e2eeacb0632e6
Mattias <mattias.linder87@gmail.com>
2015-02-14 12:08:47 +0100

    search function done and linked with home.html

--- javascript/search.js
+++ javascript/search.js
@@ -1,10 +1,15 @@
 /*
-  if Substring is a proper prefix of String, it is added to BeerList.
+  If Substring is a proper prefix of String, it is added to BeerList.
 */
 function searchString(String,SubString,BeerList){
     var length = String.length;
     var bool = true;
+    String = String.toLowerCase();
+    SubString = SubString.toLowerCase();
     for(var i = 0; i<length;i++){
+	if(String == []) {
+	    return;
+	}
 	if(String[i] === SubString[i]) {
 	    continue;
 	} else {
@@ -19,7 +24,8 @@ function searchString(String,SubString,BeerList){
 }
 
 /*
-  This functions search the database API for a beer prefix that matches the String parameter.  
+  This functions search the database API for a beer
+  prefix that matches the String parameter.  
  */
 function searchBar(String,Username,Password) {
     var url = "http://pub.jamaica-inn.net/fpdb/api.php?username=" + Username + "&password=" + Password + "&action=inventory_get";
@@ -30,5 +36,19 @@ function searchBar(String,Username,Password) {
 	for(var i = 0; i<length; i++) {
 	    searchString(String, data.payload[i].namn,beerMatched);
 	}
+	var HTML = "";
+	length = beerMatched.length;
+	for(var i = 0; i<length; i++) {
+	    HTML = HTML + "<a href=\"https://www.google.se\">" + beerMatched[i] +"</a><br>";
+	}
+	$('.ListOfBeers').html(HTML);
     });   
 }
+
+$(document).ready(function(){
+
+    $('.form-control').keyup(function(Key){
+	var String = $(this).val();
+	searchBar(String,"ervtod","ervtod");
+    }); 
+});
