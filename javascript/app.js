4c24a79d016bfcab0d20713013985ebd0314200a
TomasRingefelt <tomas.ringefelt@gmail.com>
2015-02-20 12:19:44 +0100

    Working on the orders function. Still problem with layout.

--- javascript/app.js
+++ javascript/app.js
@@ -9,11 +9,29 @@ function getAll(callback){
 }
 
 
-  $(document).ready(function(){
+(function(){
+    var order = [];
+
+  function addToOrder(data, amount, price){
+    if(data)
+      order.push({"id" : data, "amount" : amount, "price" : price });
+    console.log("Order content: ", order);
+  };
+
+  $('#inventory').delegate('.addorderbtn', 'click' , function(){
+      //console.log("Order button pressed!");
+      var itemDiv = $(this).closest('div');
+      var item_id= parseInt(itemDiv.attr('id'));
+      var item_price = parseFloat(itemDiv.find('#price').text());
+      addToOrder(item_id, 1, item_price);
+      console.log("Order button pressed!", item_id);
+  });
+    $(document).ready(function(){
       getAll(function(data){
           var tmpl = $('#listtmpl').html();
-          console.log(tmpl);
           var html = Mustache.to_html(tmpl, data);
           $('#beerlist').html(html);
       });
   });
+
+}());
