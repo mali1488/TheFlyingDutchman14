5ea2bd7be3e862bc448c5191f75dc76e03d8b8fe
Mattias <mattias.linder87@gmail.com>
2015-02-23 14:23:59 +0100

    fixed inventory

--- javascript/db.js
+++ javascript/db.js
@@ -43,7 +43,8 @@ function inventory() {
 				   item = "";
 				   if(data.payload[0] != null) {
 				       item = data.payload[0].varugrupp.toLowerCase();
-				       string = "<div class=\"col-sm-2\"><a href=\"http://pub.jamaica-inn.net/fpdb/api.php?username=ervtod&password=ervtod&action=beer_data_get&beer_id=" + data.payload[0].nr + "\" class=\"thumbnail\"><img src=\"../images/products/" + data.payload[0].namn.toLowerCase().split(' ').join('') +
+				       console.log(data.payload[0]);
+				       string = "<div class=\"col-sm-2\"><a href=\"http://pub.jamaica-inn.net/fpdb/api.php?username=ervtod&password=ervtod&action=beer_data_get&beer_id=" + data.payload[0].nr + "\" class=\"thumbnail\"><img src=\"../images/products/" + data.payload[0].nr +
 					  "_thmb.jpg\"><p style=\"font-size:x-small\">" + data.payload[0].namn +"<br>" + data.payload[0].prisinklmoms + "</p></a></div>";
 				   }
 				   console.log( data.payload[0].namn.toLowerCase().split(' ').join(''));
@@ -61,15 +62,65 @@ function inventory() {
 				       }
 				       ++beerMod;
 				   } else if (item.search("alkoholfritt") != -1){
-
+				       if(freeMod === 6) {
+					   $('#alkoholfreeList').append("<div class=\"row\" id=\"alkoholfree" +freeRowNr + "\">");
+					   freeMod = 0;
+					   ++freeRowNr;
+				       } else {
+					   $('#alkoholfree' + (freeRowNr-1)).append(string);
+					   if (freeMod === 5) {
+					       $('#alkoholfreeList').append("</div>")
+					   }
+				       }
+				       ++freeMod;
 				   } else if(item.search("rött vin") != -1) {
-
+				       if(wineMod === 6) {
+					   $('#wineList').append("<div class=\"row\" id=\"wine" +wineRowNr + "\">");
+					   wineMod = 0;
+					   ++wineRowNr;
+				       } else {
+					   $('#wine' + (wineRowNr-1)).append(string);
+					   if (wineMod === 5) {
+					       $('#wineList').append("</div>")
+					   }
+				       }
+				       ++wineMod;
 				   } else if (item.search("vitt vin") != -1) {
-				       
+				       if(wineMod === 6) {
+					   $('#wineList').append("<div class=\"row\" id=\"wine" + wineRowNr + "\">");
+					   wineMod = 0;
+					   ++wineRowNr;
+				       } else {
+					   $('#wine' + (wineRowNr-1)).append(string);
+					   if (wineMod === 5) {
+					       $('#wineList').append("</div>")
+					   }
+				       }
+				       ++wineMod;
 				   } else if (item.search("cider")) {
-				       
+				       if(ciderMod === 6) {
+					   $('#ciderList').append("<div class=\"row\" id=\"cider" + ciderRowNr + "\">");
+					   ciderMod = 0;
+					   ++ciderRowNr;
+				       } else {
+					   $('#cider' + (ciderRowNr-1)).append(string);
+					   if (ciderMod === 5) {
+					       $('#ciderList').append("</div>")
+					   }
+				       }
+				       ++ciderMod;
 				   } else {
-				       
+				        if(otherMod === 6) {
+					   $('#otherList').append("<div class=\"row\" id=\"other" + otherRowNr + "\">");
+					   otherMod = 0;
+					   ++otherRowNr;
+				       } else {
+					   $('#other' + (otherRowNr-1)).append(string);
+					   if (otherMod === 5) {
+					       $('#otherList').append("</div>")
+					   }
+				       }
+				       ++otherMod;
 				   }
 			       });
 		}
