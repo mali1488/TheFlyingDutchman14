9a4fd4155037836bb578650275e8414eadacc1dc
TomasRingefelt <tomas.ringefelt@gmail.com>
2015-02-27 14:53:11 +0100

    Updated the User service

--- js/UserService.js
+++ js/UserService.js
@@ -2,23 +2,22 @@ angular.module('Dutchman')
 .factory('User',  function UserFactory($http){
 	var dbUrl = "http://pub.jamaica-inn.net/fpdb/api.php?";
 	return {
-		authenticate: function(username, password){
-			var auth = {authenticated : false, code: -1};
-			$http.get(dbUrl + "username=" + username + "&password=" + password)
+		authenticate: function(user, callback){
+			$http.get(dbUrl + "username=" + user.name + "&password=" + user.password)
 			.success(function(data){
 				var result = data.payload[0];
 				if(result.code === 1){
-					auth = {authenticated : false, msg: "Wrong username"};
+					callback({authenticated : false, msg: "Wrong username"});
 				}
 				else if(result.code === 2){
-					auth = {authenticated : false, msg: "Wrong password"};
+					callback({authenticated : false, msg: "Wrong password"});
 				}
 				else {
-					auth = {authenticated : true, msg: "Success!"};
+					callback({authenticated : true, msg: "Success!"});
 				}
-				return auth;
 			});
 
 		}
 	};
 });
+
