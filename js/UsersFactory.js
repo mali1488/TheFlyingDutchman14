4f8882fb486b5a1467f933ec903ecb3a42bc510c
marin mikulic <shaffe@users.noreply.github.com>
2015-03-05 22:48:29 +0100

    I hate AngularJS, but in a good way.
    - productsFactory.jos
    - UserController.js
    - UsersFactory.js
    - views\user\adduser.html

--- js/UsersFactory.js
+++ js/UsersFactory.js
@@ -1,10 +1,7 @@
-// Data service for all users
+// Data service for products
 
 angular.module('Dutchman')
 .factory('Users', function ProductsFactory($http){
 	var dbUrl = "http://pub.jamaica-inn.net/fpdb/api.php?username=ervtod&password=ervtod";
-	return {
-		all: function(){
-			return $http.get(dbUrl + "&action=iou_get_all");
-		}
-}});
+	
+});
